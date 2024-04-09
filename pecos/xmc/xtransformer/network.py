9dda9e5c71dc8c83357a5813bc0427dbcea6701c
TestOver50 <164345542+TestOver50@users.noreply.github.com>
2024-04-09 17:23:00 +0100

    make it to allow for deberta v2 transformers

--- pecos/xmc/xtransformer/network.py
+++ pecos/xmc/xtransformer/network.py
@@ -21,6 +21,10 @@ from transformers import (
     RobertaConfig,
     RobertaModel,
     RobertaTokenizerFast,
+    DebertaV2Config,
+    DebertaV2Model,
+    DebertaV2TokenizerFast,
+    DebertaPreTrainedModel,
     XLMRobertaConfig,
     XLMRobertaTokenizerFast,
     XLNetConfig,
@@ -514,36 +518,35 @@ class DistilBertForXMC(DistilBertPreTrainedModel):
             "hidden_states": instance_hidden_states,
         }
 
-class DebertaV2ForXMC(DebertaV2PreTrainedModel):
+class DebertaForXMC(DebertaPreTrainedModel):
     """
     Examples:
-        tokenizer = DeRobertaV2Tokenizer.from_pretrained('DeRobertaV2-base')
-        model = DeRobertaV2ForXMC.from_pretrained('DeRobertaV2-base')
+        tokenizer = DeRobertaTokenizer.from_pretrained('DeRoberta-base')
+        model = DeRobertaForXMC.from_pretrained('DeRoberta-base')
         input_ids = torch.tensor(tokenizer.encode("iphone 11 case", add_special_tokens=True)).unsqueeze(0)
         outputs = model(input_ids)
         last_hidden_states = outputs["hidden_states"]
     """
 
     def __init__(self, config):
-        super(DebertaV2ForXMC, self).__init__(config)
+        super(DebertaForXMC, self).__init__(config)
         self.num_labels = config.num_labels
 
-        self.debertaV2 = DebertaV2Model(config)
+        self.deberta = DebertaV2Model(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
         self.init_weights()
 
     def init_from(self, model):
-        self.debertaV2 = model.debertaV2
+        self.deberta = model.deberta
 
-    @add_start_docstrings(ROBERTA_INPUTS_DOCSTRING.format("(batch_size, sequence_length)"))
+
     def forward(
         self,
         input_ids=None,
         attention_mask=None,
         token_type_ids=None,
         position_ids=None,
-        head_mask=None,
         inputs_embeds=None,
         label_embedding=None,
     ):
@@ -556,12 +559,11 @@ class DebertaV2ForXMC(DebertaV2PreTrainedModel):
                 }
         """
 
-        outputs = self.debertaV2(
+        outputs = self.deberta(
             input_ids,
             attention_mask=attention_mask,
             token_type_ids=token_type_ids,
             position_ids=position_ids,
-            head_mask=head_mask,
             inputs_embeds=inputs_embeds,
             return_dict=True,
         )
@@ -580,14 +582,15 @@ class DebertaV2ForXMC(DebertaV2PreTrainedModel):
             "pooled_output": pooled_output,
             "hidden_states": instance_hidden_states,
         }
-        
+
+
 ENCODER_CLASSES = {
     "bert": TransformerModelClass(BertConfig, BertForXMC, BertTokenizerFast),
     "roberta": TransformerModelClass(RobertaConfig, RobertaForXMC, RobertaTokenizerFast),
     "xlm-roberta": TransformerModelClass(
         XLMRobertaConfig, XLMRobertaForXMC, XLMRobertaTokenizerFast
     ),
-    "deberta-v2": TransformerModelClass(DebertaV2Config,DebertaV2ForXMC,DebertaV2TokenizerFast),
+    "deberta-v2": TransformerModelClass(DebertaV2Config,DebertaForXMC,DebertaV2TokenizerFast),
     "xlnet": TransformerModelClass(XLNetConfig, XLNetForXMC, XLNetTokenizerFast),
     "distilbert": TransformerModelClass(
         DistilBertConfig, DistilBertForXMC, DistilBertTokenizerFast
